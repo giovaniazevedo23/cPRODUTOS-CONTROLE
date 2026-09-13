de297f506b704e849e4c45cfd642a1a187983363
giovaniazevedo23 <giovani.azevedo06@gmail.com>
2026-09-13 01:56:30 -0300

    fix: express 5 fallback routing wildcard

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ app.use('/mp-api', async (req, res) => {
 app.use(express.static(path.join(__dirname, 'dist')));
 
 // Fallback to index.html for React Router
-app.get('*', (req, res) => {
+app.get(/.*/, (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
